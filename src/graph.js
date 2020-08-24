508d3b0c3a4623a9507eb735c097718a9deec386
Alexis9990 <66976107+Alexis9990@users.noreply.github.com>
2020-08-24 14:53:47 -0500

    Update graph.js

--- src/graph.js
+++ src/graph.js
@@ -49,7 +49,7 @@ class Graph extends React.Component {
       graphs: []
     }, () => { // La función setState tiene un callback dado que es una función asíncrona de JS, para asegurarse que lo que se quiera hacer después de cambiar el estado se debe usar el callback
       this.createGraph() // Después de cargar la información en memoria se pasa a crear los grafos
-      $("#cooScale").val(1)
+      $("#cooScale").val(10)
     })
   }
 
@@ -247,7 +247,7 @@ class Graph extends React.Component {
     })
   }
 
-  scaleCoordinates = () =>{ // Nos permite ajustar la escala de los grafos
+  scaleCoordinates = (10) =>{ // Nos permite ajustar la escala de los grafos
     let scale = document.getElementById("cooScale").value // Obtenemos el valor seleccionado desde el DOM
 
     if(scale < 10 || scale > 20){ // Evitamos que se usen valores fuera de rango
@@ -260,7 +260,7 @@ class Graph extends React.Component {
     // Dado que tenemos el valor de la escala guardada en memoria lo que hacemos es 
     // remover la escala que se tenga actualmente para volver a los datos reales y poder aplicar la escala seleccionada
 
-    for(let i=10; i<=this.state.maxId; i++){
+    for(let i=1; i<=this.state.maxId; i++){
       data[i]["nodes"].forEach(node => {
         node.fx /= this.state.scale
         node.fy /= this.state.scale
