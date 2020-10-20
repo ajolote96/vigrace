1090f4b5ae3b72783d3ad3efb4f5ecd32b1447a2
Alexis9990 <66976107+Alexis9990@users.noreply.github.com>
2020-10-20 12:15:28 -0500

    Update graph.js

--- src/graph.js
+++ src/graph.js
@@ -342,6 +342,11 @@ class Graph extends React.Component {
       pauseMoments: false
     })
   }
+  bakcgroundBlack = () =>{
+    this.setState({
+      backgroundColor={"#919191"}
+                  })
+     }
 
   render() { // El método render debe devolver un solo objeto sugar HTML, que puede ser cualquiera y con lo que sea dentro
     return (
