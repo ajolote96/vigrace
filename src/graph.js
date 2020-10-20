bea5a48bf2154d348c3429dbbde80a5ceb2ec268
Alexis9990 <66976107+Alexis9990@users.noreply.github.com>
2020-10-20 13:28:11 -0500

    Update graph.js

--- src/graph.js
+++ src/graph.js
@@ -197,7 +197,7 @@ class Graph extends React.Component {
       graphData={this.state.data[this.state.currentId]}
       rendererConfig={{preserveDrawingBuffer: true}}
       nodeResolution={200}
-      backgroundColor={"#919191"}
+     // backgroundColor={"#919191"}
       nodeVal={nod => sizes[this.state.currentId - 1][nod.id] }
       nodeColor={() => this.state.colors[this.state.currentId - 1]}
       nodeLabel={"name"}
